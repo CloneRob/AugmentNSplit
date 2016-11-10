b8598d81de35bade533004d915ae7ced40cfea71
CloneRob <ropamen@gmail.com>
2016-11-10 16:52:16 +0100

    Updated the dependencies to ensure compilation

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@
 #![feature(step_by)]
 
 extern crate image;
-extern crate xml;
 extern crate rand;
-extern crate time;
 
 mod ans;
 mod img_reader;
@@ -12,7 +10,7 @@ mod img_reader;
 // use std::fs::File;
 use std::path::PathBuf;
 // use std::collections::HashMap;
-use time::PreciseTime;
+//use time::PreciseTime;
 
 use img_reader::{ImgReader, LabelType};
 
@@ -70,11 +68,6 @@ impl FindLabel for Oversample {
 
 
 fn main() {
-    // let config_path = PathBuf::from("/home/robert/Projects/rust/AugmentNSplit/test_config.xml");
-
-    // let training_path = PathBuf::from("/media/robert/Lokaler Datenträger/BachelorArbeit/Bilder/images");
-    // let label_path = PathBuf::from("/media/robert/Lokaler Datenträger/BachelorArbeit/Bilder/mask_and");
-
     let training_path = PathBuf::from("/media/robert/Lokaler \
                                        Datenträger/BachelorArbeit/Bilder/subset");
     let label_path = PathBuf::from("/media/robert/Lokaler \
@@ -82,7 +75,7 @@ fn main() {
 
     let label_type = LabelType::Img(label_path);
 
-    let now = PreciseTime::now();
+    //let now = PreciseTime::now();
     let mut augment_split = AugmentSplitBuilder::new()
         .set_img_dir(training_path)
         .set_label_type(label_type)
@@ -95,19 +88,19 @@ fn main() {
         .build();
 
 
-    let finish = PreciseTime::now();
-    let duration = now.to(finish);
-    println!("{:?} ns to create Ans struct", duration.num_nanoseconds());
+    //let finish = PreciseTime::now();
+   // let duration = now.to(finish);
+    //println!("{:?} ns to create Ans struct", duration.num_nanoseconds());
 
-    let now = PreciseTime::now();
+    //let now = PreciseTime::now();
     let mut img_reader = ImgReader::new(augment_split.get_imgdir(), augment_split.get_label_type());
-    let finish = PreciseTime::now();
-    let duration = now.to(finish);
-    println!("{:?} ms to create img_reader", duration.num_milliseconds());
+    //let finish = PreciseTime::now();
+    //let duration = now.to(finish);
+    //println!("{:?} ms to create img_reader", duration.num_milliseconds());
 
 
 
-    let now = PreciseTime::now();
+    //let now = PreciseTime::now();
     let mut s = Split { ratio: None };
     let cv = color_values::ColorValues::white_luma();
 
@@ -115,39 +108,7 @@ fn main() {
 
     let mut os = Oversample { ratio: None };
     augment_split.oversample(&mut img_reader, 0.0004, cv, &mut os);
-    let finish = PreciseTime::now();
-    let duration = now.to(finish);
-    println!("{:?} ms to split images", duration.num_milliseconds());
-
-    // let now = PreciseTime::now();
-    // augment_split.build_sick(&mut img_reader);
-    // let finish = PreciseTime::now();
-    // let duration = now.to(finish);
-    // println!("{:?} ms to split images", duration.num_milliseconds());
-    // let now = PreciseTime::now();
-    // ans.build_healthy(&mut img_reader);
-    // let finish = PreciseTime::now();
-    // let duration = now.to(finish);
-    // println!("{:?} ms to split Healthy images", duration.num_milliseconds());
-    //
-    // let now = PreciseTime::now();
-    // ans.build_sick(&mut img_reader);
-    // let finish = PreciseTime::now();
-    // let duration = now.to(finish);
-    // println!("{:?} ms to split Sick images", duration.num_milliseconds());
-    //
-    // let path = Path::new("/media/robert/Lokaler Datenträger/BachelorArbeit/Bilder/training_data/pat18_im3_ACHD_0_960.png");
-    // let mut img = open(&path).unwrap().to_rgb();
-    // let img = imageops::crop(&mut img, 0, 0, 224, 224);
-    //
-    // let color_info = Ans::get_color([0, 0, 0], &img);
-    //
-    // let pixels = {
-    // let p = img.dimensions();
-    // p.0 * p.1
-    // };
-    //
-    // println!("Pixels: {}, black pixels {}, ratio {}", pixels, color_info.1, color_info.1 / pixels as f32);
-    //
-
+    //let finish = PreciseTime::now();
+    //let duration = now.to(finish);
+    //println!("{:?} ms to split images", duration.num_milliseconds());
 }
